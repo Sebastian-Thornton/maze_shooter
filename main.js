089f3cfffb6b8c0585db8f3f8d832fc22fe43430
Cheerio2 <sebastian.j.thornton@outlook.com>
2025-11-09 18:49:25 +0000

    Physics is now working with Babylon, as is realistic lighting.

--- main.js
+++ main.js
@@ -8,13 +8,24 @@ var createScene = function () {
   camera.setTarget(BABYLON.Vector3.Zero());
   camera.attachControl(canvas, true);
 
-  var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+  scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.CannonJSPlugin());
+
+  var light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(-1, -2, -1), scene);
+  light.position = new BABYLON.Vector3(20, 40, 20);
+  light.shadowEnabled = true;
   light.intensity = 0.7;
 
   var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2}, scene);
-  sphere.position.y = 1;
+  sphere.position.y = 2;
 
   var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+
+  sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
+  ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
+
+  var shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
+  shadowGenerator.addShadowCaster(sphere);
+  ground.receiveShadows = true;
 
   return scene;
 };
