373e4f54af391bc6c3deb5f100af57decde3c29c
Cheerio2 <sebastian.j.thornton@outlook.com>
2025-11-20 15:08:24 +0000

    It doesn't work at the moment, so I need to sort that out. Maybe compartmentalise the programming a bit.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var canvas = document.getElementById("renderCanvas");
-var engine = new BABYLON.Engine(canvas, true);
+let canvas = document.getElementById("renderCanvas");
+let engine = new BABYLON.Engine(canvas, true);
 
-await window.CrazyGames.SDK.init();
+// await window.CrazyGames.SDK.init();
 
 let level_data
 
@@ -10,36 +10,74 @@ level_data = await response.json();
 
 console.log(level_data);
 
-var createScene = function () {
-  var scene = new BABYLON.Scene(engine);
 
-  var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-  camera.setTarget(BABYLON.Vector3.Zero());
-  camera.attachControl(canvas, true);
 
-  scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.CannonJSPlugin());
+let loadLevel = function (level_number) {
+    console.log(level_data);
+    map = new Array(level_data.level[level_number].grid.length);
+    for(let z = 0; z<level_data.level[level_number].grid.length; z++) {
+        map[z] = new Array(level_data.level[level_number].grid[0].length);
+        for(let y = 0; y<level_data.level[level_number].grid[0].length; y++) {
+            map[z][y] = new Array(level_data.level[level_number].grid[0][0].length);
+            for(let x = 0; x<level_data.level[level_number].grid[0][0].length; x++) {
+                if (level_data.level[level_number].grid[0][0].length == 1) {
+                    map[x][y][z] = BABYLON.MeshBuilder.CreateBox("box",1,1,1,scene);
+                }
+            }
+        }
+    }
+}
 
-  var light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(-1, -2, -1), scene);
-  light.position = new BABYLON.Vector3(20, 40, 20);
-  light.shadowEnabled = true;
-  light.intensity = 0.7;
 
-  var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2}, scene);
-  sphere.position.y = 2;
 
-  var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+let createScene = function () {
 
-  sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
-  ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
 
-  var shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
-  shadowGenerator.addShadowCaster(sphere);
-  ground.receiveShadows = true;
 
-  return scene;
+    let scene = new BABYLON.Scene(engine);
+
+    let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.attachControl(canvas, true);
+
+    scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.CannonJSPlugin());
+
+    let light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(-1, -2, -1), scene);
+    light.position = new BABYLON.Vector3(20, 40, 20);
+    light.shadowEnabled = true;
+    light.intensity = 0.7;
+
+    /* map = new Array(level_data[level_number].grid.length);
+    for(let z = 0; z<level_data.level[level_number].grid.length; z++) {
+        map[z] = new Array(level_data.level[level_number].grid[0].length);
+        for(let y = 0; y<level_data.level[level_number].grid[0].length; y++) {
+            map[z][y] = new Array(level_data.level[level_number].grid[0][0].length);
+            for(let x = 0; x<level_data.level[level_number].grid[0][0].length; x++) {
+                if (level_data.level[level_number].grid[0][0].length == 1) {
+                    map[x][y][z] = BABYLON.MeshBuilder.CreateBox("box",1,1,1,scene);
+                }
+            }
+        }
+    } */
+    
+    loadLevel(level_data, 0);
+
+    let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2}, scene);
+    sphere.position.y = 2;
+
+    let ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+
+    sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
+    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
+
+    let shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
+    shadowGenerator.addShadowCaster(sphere);
+    ground.receiveShadows = true;
+
+    return scene;
 };
 
-var scene = createScene();
+let scene = createScene();
 engine.runRenderLoop(function () {
-  scene.render();
+    scene.render();
 });
